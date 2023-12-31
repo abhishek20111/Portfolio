54a2567a41cf95b6153bfa035928ef01136f4050
Whitecoder <86569824+abhishek20111@users.noreply.github.com>
2023-12-31 15:49:08 +0530

    i have done some new changes in new branch falldown .

--- src/Service.jsx
+++ src/Service.jsx
@@ -4,7 +4,7 @@ import './App.css'
 export default function Service() {
   return (
     <div className="bg-blue-200 md:mt-2 mt-16 sm:mt-4  h-screen  md:h-[90vh] fullGetScreen">
-      service
+      service done
     </div>
   )
 }
